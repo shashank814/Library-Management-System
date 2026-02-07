49c7a2a25fdbbfd0408d8e50814397bc2d93542b
Shashank Singh <sshingh456@gmail.com>
2026-02-07 13:35:31 +0530

    feat: update API endpoints to use localhost for development and remove Vercel configuration files

--- client/src/store/slices/borrowSlice.js
+++ client/src/store/slices/borrowSlice.js
@@ -78,7 +78,7 @@ const borrowSlice = createSlice({
 
 export const fetchUserBorrowedBooks = () => async(dispatch) => {
     dispatch(borrowSlice.actions.fetchUserBorrowedBooksRequest());
-    await axios.get("https://library-management-system-server-one.vercel.app//api/v1/borrow/my-borrowed-books", { withCredentials: true })
+    await axios.get("http://localhost:4000//api/v1/borrow/my-borrowed-books", { withCredentials: true })
     .then((res) => {
         dispatch(borrowSlice.actions.fetchUserBorrowedBooksSuccess(res.data.borrowedBooks))
     }).catch((err) => {
@@ -88,7 +88,7 @@ export const fetchUserBorrowedBooks = () => async(dispatch) => {
 
 export const fetchAllBorrowedBooks = () => async(dispatch) => {
     dispatch(borrowSlice.actions.fetchAllBorrowedBooksRequest());
-    await axios.get("https://library-management-system-server-one.vercel.app//api/v1/borrow/borrowed-books-by-users", { withCredentials: true })
+    await axios.get("http://localhost:4000//api/v1/borrow/borrowed-books-by-users", { withCredentials: true })
     .then((res) => {
         dispatch(borrowSlice.actions.fetchAllBorrowedBooksSuccess(res.data.borrowedBooks))
     }).catch((err) => {
@@ -98,7 +98,7 @@ export const fetchAllBorrowedBooks = () => async(dispatch) => {
 
 export const recordBorrowBook = (email, bookId) => async (dispatch) => {
     dispatch(borrowSlice.actions.recordBookRequest());
-    await axios.post(`https://library-management-system-server-one.vercel.app//api/v1/borrow/record-borrow-book/${bookId}`, {email}, {
+    await axios.post(`http://localhost:4000//api/v1/borrow/record-borrow-book/${bookId}`, {email}, {
         withCredentials: true,
         headers: {
             "Content-Type": "application/json"
@@ -113,7 +113,7 @@ export const recordBorrowBook = (email, bookId) => async (dispatch) => {
 
 export const returnBook = (email, bookId) => async(dispatch) => {
     dispatch(borrowSlice.actions.returnBookRequest());
-    await axios.put(`https://library-management-system-server-one.vercel.app//api/v1/borrow/return-borrowed-book/${bookId}`, {email}, {
+    await axios.put(`http://localhost:4000//api/v1/borrow/return-borrowed-book/${bookId}`, {email}, {
         withCredentials: true,
         headers: {
             "Content-Type": "application/json",
